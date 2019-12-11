2f64ab29dc48e8c47f7296a515cf3301914a2d5b
Knut Ahlers <knut@ahlers.me>
2019-12-11 19:23:17 +0100

    Fix: Profit logs contain pointer, not strings
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- block_profit_log_entry.go
+++ block_profit_log_entry.go
@@ -5,19 +5,19 @@ func init() {
 }
 
 type ProfitLogEntry struct {
-	Revenue            int64  `sii:"revenue"`
-	Wage               int64  `sii:"wage"`
-	Maintenance        int64  `sii:"maintenance"`
-	Fuel               int64  `sii:"fuel"`
-	Distance           int64  `sii:"distance"`
-	DistanceOnJob      bool   `sii:"distance_on_job"`
-	CargoCount         int64  `sii:"cargo_count"`
-	Cargo              string `sii:"cargo"`
-	SourceCity         string `sii:"source_city"`
-	SourceCompany      string `sii:"source_company"`
-	DestinationCity    string `sii:"destination_city"`
-	DestinationCompany string `sii:"destination_company"`
-	TimestampDay       int64  `sii:"timestamp_day"`
+	Revenue            int64 `sii:"revenue"`
+	Wage               int64 `sii:"wage"`
+	Maintenance        int64 `sii:"maintenance"`
+	Fuel               int64 `sii:"fuel"`
+	Distance           int64 `sii:"distance"`
+	DistanceOnJob      bool  `sii:"distance_on_job"`
+	CargoCount         int64 `sii:"cargo_count"`
+	Cargo              Ptr   `sii:"cargo"`
+	SourceCity         Ptr   `sii:"source_city"`
+	SourceCompany      Ptr   `sii:"source_company"`
+	DestinationCity    Ptr   `sii:"destination_city"`
+	DestinationCompany Ptr   `sii:"destination_company"`
+	TimestampDay       int64 `sii:"timestamp_day"`
 
 	blockName string
 }
