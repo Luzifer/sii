55696d1c77e15234d85043650abb96558ab4ce8a
Knut Ahlers <knut@ahlers.me>
2019-12-23 17:18:09 +0100

    Add list method for saves
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- cmd/sii-editor/api_profiles.go
+++ cmd/sii-editor/api_profiles.go
@@ -5,9 +5,18 @@ import "net/http"
 func init() {
 	router.HandleFunc("/api/profiles", handleListProfiles).Methods(http.MethodGet)
 	router.HandleFunc("/api/profiles/{profileID}", handleGetProfileInfo).Methods(http.MethodGet)
+	router.HandleFunc("/api/profiles/{profileID}/saves", handleGetProfileSaves).Methods(http.MethodGet)
 }
 
 func handleGetProfileInfo(w http.ResponseWriter, r *http.Request) {
+	// FIXME: Implementation missing
+}
+
+func handleGetProfileSaves(w http.ResponseWriter, r *http.Request) {
+	var subscribe = r.FormValue("subscribe") == "true"
+
+	_ = subscribe // If so open socket and let browser know there are new saves
+
 	// FIXME: Implementation missing
 }
 
