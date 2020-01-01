bbbce2b2a36a4a928d902b0b8cba413a68fa0674
Knut Ahlers <knut@ahlers.me>
2020-01-01 01:26:51 +0100

    Fix: TotalTransportedWeight seems to be float
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- block_trailer_utilization_log.go
+++ block_trailer_utilization_log.go
@@ -5,10 +5,10 @@ func init() {
 }
 
 type TrailerUtilizationLog struct {
-	Entries                 []Ptr `sii:"entries"` // Needs verification
-	TotalDrivenDistanceKM   int64 `sii:"total_driven_distance_km"`
-	TotalTransportedCargoes int64 `sii:"total_transported_cargoes"`
-	TotalTransportedWeight  int64 `sii:"total_transported_weight"`
+	Entries                 []Ptr   `sii:"entries"` // Needs verification
+	TotalDrivenDistanceKM   int64   `sii:"total_driven_distance_km"`
+	TotalTransportedCargoes int64   `sii:"total_transported_cargoes"`
+	TotalTransportedWeight  float32 `sii:"total_transported_weight"`
 
 	blockName string
 }
